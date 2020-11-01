da8e6d4fe2f0b1d9c31a9f6f06012788469cb791
Cyborg500zx <lameira.alberto94@gmail.com>
2020-11-01 19:54:45 +0000

    gun, shoot, animations

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(20.0f, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player")
+        {
+            // run impact animation, then destroy
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,14 @@ public class Player : MonoBehaviour
     private Animator _anim;
     private SpriteRenderer _renderer;
     private bool _isFacingLeft;
-    private GameObject _flashlight;
     [HideInInspector]
     public bool _canUseDoor;
+    private bool _isAiming = false;
+    private bool _flashlightOn = false;
+    private GameObject _flashlight;
+    private GameObject _pistol;
+    [SerializeField]
+    public GameObject _bulletRef;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +31,8 @@ public class Player : MonoBehaviour
         _anim = gameObject.GetComponent<Animator>();
         _renderer = gameObject.GetComponent<SpriteRenderer>();
         _flashlight = gameObject.transform.GetChild(0).gameObject;
+        _flashlight.transform.parent = this.gameObject.transform;
+        _pistol = gameObject.transform.GetChild(1).gameObject;
 
         if (_rigidBody == null)
 
@@ -61,7 +68,78 @@ public class Player : MonoBehaviour
             NS_GameManager.GameManager.LoadSceneFromDoor();
         }
 
+        if (Input.GetButtonDown("Fire2"))
+        {
+            _isAiming = true;
+            NS_GameManager.GameManager.Aim();
+            ToggleAim();
+        }
+
+        if (Input.GetButtonUp("Fire2"))
+        {
+            _isAiming = false;
+            NS_GameManager.GameManager.NoAim();
+            ToggleAim();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ToggleFlashLight();
+        }
+
+        if (_isAiming && Input.GetButtonDown("Fire1"))
+        {
+            // shoot
+            Shoot();
+        }
+
+        // TODO: Implement dynamic arm system, points to where cursor is pointing.
+        /*var mouse = Input.mousePosition;
+        var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);*/
+
         MoveCharacter();
+    }
+
+    void Shoot()
+    {
+        // TODO: Use "Pistol_Barrel" as reference point of Instantiate instead of pistol, avoids hardcoded offset values,
+        // good for more weapon implementation and fine tuning bullet spawn point
+        GameObject bullet = Instantiate(_bulletRef, _pistol.transform.position + new Vector3(0.18f, 0.06f, 0), _pistol.transform.rotation, _pistol.transform);
+        bullet.transform.localScale = new Vector3(0.3f, 0.3f, 0);
+    }
+
+    void ToggleAim()
+    {
+        if (_isAiming)
+        {
+            // run aim animation
+            _anim.SetBool("isAiming", true);
+            _pistol.SetActive(true);
+        }
+        else
+        {
+            // run no aim animation
+            _anim.SetBool("isAiming", false);
+            _pistol.SetActive(false);
+        }
+    }
+
+    void ToggleFlashLight()
+    {
+        if (_flashlightOn)
+        {
+            _flashlightOn = false;
+            _flashlight.SetActive(false);
+        } 
+        else
+        {
+            _flashlightOn = true;
+            _flashlight.SetActive(true);
+        }
+
     }
 
     void MoveCharacter()
