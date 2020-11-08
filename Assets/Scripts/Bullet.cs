e0c2291b01132e0505b22c41e22a1cc6ba7244cb
Cyborg500zx <lameira.alberto94@gmail.com>
2020-11-08 20:42:44 +0000

    enemy_v0.1, some level detail, reload, UI system

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,12 +10,6 @@ public class Bullet : MonoBehaviour
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(20.0f, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag != "Player")
---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int _health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Damage()
+    {
+        _health -= 10;
+
+        if (_health <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        //run death animation, then destroy
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Debug.Log("hello");
+            // run hurt animation, then apply damage
+            Damage();
+        }
+    }
+}
